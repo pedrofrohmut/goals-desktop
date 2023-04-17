87e6622d4288589ba2286bfcc017560156731812
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-17 10:50:57 -0300

    Dashboard UI no functionalities

--- src/renderer/src/pages/DashboardPage.tsx
+++ src/renderer/src/pages/DashboardPage.tsx
@@ -1,8 +1,46 @@
+import { useState } from "react"
+
+import Spinner from "../components/Spinner"
+import AddGoalForm from "../components/AddGoalForm"
+import GoalList from "../components/GoalList"
+
+// TEMP:
+const isLoadingGoals = false
+const goals = [
+    { id: 1, text: "Goal 1" },
+    { id: 2, text: "Goal 2" },
+    { id: 3, text: "Goal 3" },
+]
+const user = {
+    email: "john@doe.com",
+    name: "John Doe"
+}
+
 const DashboardPage = () => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleAdd = () => {}
+
+    const handleDelete = () => {}
+
     return (
-        <div className="page-container">
-            <h1>Dashboard Page</h1>
-        </div>
+        <>
+            {(isLoading || isLoadingGoals) && <Spinner />}
+
+            <div className="page-container">
+                <section className="heading">
+                    <h1 className="page-title">
+                        <i className="fa-solid fa-calendar-days"></i>
+                        Goals Dashboard
+                    </h1>
+                    <p>Welcome {user && user.name ? user.name : "username"}</p>
+                </section>
+
+                <AddGoalForm addGoal={handleAdd} />
+
+                <GoalList goals={goals} removeGoal={handleDelete} />
+            </div>
+        </>
     )
 }
 
